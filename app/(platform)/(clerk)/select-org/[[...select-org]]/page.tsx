88f5c45479ef086de51a766aa69739cfaaa803e2
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-11-29 18:20:52 +0200

    ADD page.tsx

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,9 @@
+import { OrganizationList } from "@clerk/nextjs"
+
+export default function CreateOrganizationPage() {
+    return <OrganizationList
+        hidePersonal
+        afterSelectOrganizationUrl={"/organization/:id"}
+        afterCreateOrganizationUrl={"/organization/:id"}
+    />
+}
