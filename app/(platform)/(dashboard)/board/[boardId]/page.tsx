6450aa191a2f27519fff2230917c36e4753d4d38
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-12-06 13:04:07 +0200

    UPD page.tsx & schema.prisma & site.ts

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,8 +1,34 @@
+import {auth} from "@clerk/nextjs";
+import {db} from "@/lib/db";
+import {redirect} from "next/navigation";
 
-export default function BoardIdPage() {
+type BoardIdPageProps = {
+    params: {
+        boardId: string,
+    },
+}
+
+export default async function BoardIdPage({ params: { boardId } }: BoardIdPageProps) {
+    const {orgId} = auth();
+    if (!orgId) redirect("/select-org");
+
+    const lists = await db.list.findMany({
+        where: {
+            boardId,
+            board: {
+                orgId,
+            }
+        },
+        include: {
+            Card: true,
+        },
+    });
+
+
+
     return (
-        <div>
+        <>
 
-        </div>
+        </>
     )
 }
--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,4 @@
 export const siteConfig = {
     title: 'Taskify',
     description: 'Collaborate, manage projects, and reach new productivity peaks.',
-
 }
